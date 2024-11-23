a4e6896997991a8d138fd9df9307a3886e16638d
marcin.chowaniec <marcin.chowaniec@asseco.pl>
2024-11-23 16:23:17 +0100

    Add form component, update routing, and enhance city details with coordinates

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -10,10 +10,13 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 export default function CityItem({ city }) {
-  const { cityName, country, emoji, date } = city;
+  const { cityName, country, emoji, date, id, position } = city;
   return (
     <li>
-      <Link className={styles.cityItem} to={`${city.id}`}>
+      <Link
+        className={styles.cityItem}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,27 @@
 import React from "react";
 import styles from "./Map.module.css";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export default function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>Map</h1>
+      <p>
+        Position {lat}, {lng}
+      </p>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 40.73061, lng: -73.935242 });
+        }}
+      >
+        Change position
+      </button>
+    </div>
+  );
 }
