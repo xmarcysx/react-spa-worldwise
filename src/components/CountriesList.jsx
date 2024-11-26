400cf38e121ecf87614f9ff236201dff1c762d07
marcin.chowaniec <marcinchowaniec21@gmail.com>
2024-11-26 20:21:51 +0100

    Refactor city and country components to use context for state management, add BackButton component, and enhance loading states

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -3,8 +3,11 @@ import styles from "./CountryList.module.css";
 import Spinner from "./Spinner";
 import CountryItem from "./CountryItem";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
-export default function CountriesList({ isLoading, cities }) {
+export default function CountriesList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) {
     return <Spinner></Spinner>;
   }
