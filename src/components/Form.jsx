d841ae8603c6b2383a681dce964d99b8cdf4d3f3
marcin.chowaniec <marcin.chowaniec@asseco.pl>
2024-12-09 20:49:53 +0100

    Add geolocation and URL position hooks, enhance Form and Map components with dynamic city data fetching and user location features

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,10 @@
-// "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useUrlPosition } from "../hooks/useUrlPosition";
 
 import styles from "./Form.module.css";
 import Button from "./Button";
 import BackButton from "./BackButton";
+import Spinner from "./Spinner";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -14,11 +14,45 @@ export function convertToEmoji(countryCode) {
   return String.fromCodePoint(...codePoints);
 }
 
+const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
+
 function Form() {
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
+  const [emoji, setEmoji] = useState("");
+  const [isLoadingGeaocoding, setIsLoadingGeocoding] = useState(false);
+  const [lat, lng] = useUrlPosition();
+
+  useEffect(
+    function () {
+      async function fetchCityData() {
+        try {
+          setIsLoadingGeocoding(true);
+          const res = await fetch(
+            `${BASE_URL}?latitude=${lat}&longitude=${lng}`
+          );
+          const data = await res.json();
+
+          if (!data.countryCode) {
+            throw new Error("City not found");
+          }
+
+          setCityName(data.city || data.locality || "");
+          setCountry(data.countryName || "");
+          setEmoji(convertToEmoji(data.countryCode || ""));
+        } catch (err) {
+        } finally {
+          setIsLoadingGeocoding(false);
+        }
+      }
+      fetchCityData();
+    },
+    [lat, lng]
+  );
+
+  if (isLoadingGeaocoding) return <Spinner />;
 
   return (
     <form className={styles.form}>
@@ -29,7 +63,7 @@ function Form() {
           onChange={(e) => setCityName(e.target.value)}
           value={cityName}
         />
-        {/* <span className={styles.flag}>{emoji}</span> */}
+        <span className={styles.flag}>{emoji}</span>
       </div>
 
       <div className={styles.row}>
